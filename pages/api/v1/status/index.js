85434b9efbca0f62dbef6bd00e4f757a4cd1c66a
Bruno Faboci <38674084+brunofaboci@users.noreply.github.com>
2025-01-02 20:20:43 +0000

    added database info to status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,36 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  const maxConnectionResult = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionResult.rows[0].max_connections;
+
+  //const openConnectionsResult = await database.query(
+  //  "SELECT sum(numbackends) FROM pg_stat_database;",
+  //);
+  //const openConnections = openConnectionsResult.rows[0].sum;
+
+  const databaseName = process.env.POSTGRES_DB
+  const openConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+  const openConnections = openConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(maxConnections),
+        open_connections: openConnections,
+      },
+    },
+  });
 }
 
 export default status;
